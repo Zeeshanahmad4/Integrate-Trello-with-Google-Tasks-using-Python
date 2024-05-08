24a5080ccddbe445e7d96ff0b1ad32419e71802d
Zeeshan Ahmad <ahmadzee26@gmail.com>
2024-05-08 16:23:31 +0500

    Update __init__.py

---
+++ config/__init__.py
@@ -0,0 +1,9 @@
+# config/__init__.py
+
+from .settings import TRELLO_API_KEY, TRELLO_API_SECRET, GOOGLE_CLIENT_SECRETS_FILE
+
+__all__ = [
+    'TRELLO_API_KEY', 
+    'TRELLO_API_SECRET', 
+    'GOOGLE_CLIENT_SECRETS_FILE'
+]
